097d2fc56eb4d4f755caa4968c8f9ae7453a2f43
Lucas Miguel Santos <lucasmiguel@MacBook-Air-de-Lucas.local>
2025-02-25 01:00:18 -0300

    change api path

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://backend-calculadorarescisaotrabalho.onrender.com/api';
 
 export const calculateRescisao = async (data) => {
   const response = await axios.post(`${API_BASE_URL}/calculations`, data);
